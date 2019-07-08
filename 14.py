d2edb3cb74c0aab30452085a14f5e77a9f2a430a
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2019-07-08 16:17:34 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -3,6 +3,6 @@ vr=list(map(str,input()[:a]))
 oi=['a','e','i','o','u','A','E','I','O','U']
 rr=[]
 for i in vr:
-   if i not in rr:
+   if i not in oi:
       rr.append(i)
 print(''.join(map(str,rr[::-1])))      
